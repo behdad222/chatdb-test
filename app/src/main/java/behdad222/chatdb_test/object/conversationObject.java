c8068bebb8dd4ca6d45264fe496a97928cc40f4d
behdad222 <behdad.222@gmail.com>
2015-04-01 02:28:13 +0430

    add conarsation object

---
+++ app/src/main/java/behdad222/chatdb_test/object/conversationObject.java
@@ -0,0 +1,64 @@
+package behdad222.chatdb_test.object;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by behdad on 4/1/15.
+ */
+
+public class conversationObject extends RealmObject {
+    private double id;
+    private String conversationId;
+    private String body;
+    private int type;
+    private int status;
+    private String date;
+
+    public double getId() {
+        return id;
+    }
+
+    public void setId(double id) {
+        this.id = id;
+    }
+
+    public String getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(String conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
